b2cef76c1b5bc8f3e941767e8e5a5e9aa8cbf9d1
diewland <diewland@gmail.com>
2022-10-06 07:59:12 +0700

    revise structure

---
+++ 015_doggo.py
@@ -0,0 +1,37 @@
+import json
+import os
+ 
+TOKEN_SIZE = 10_000
+START_ID = 0
+OUTPUT_DIR = os.path.basename(__file__).split('.')[0]
+
+NAME = "Doggo 🦮"
+DESC = "Beware of Doggo 🦮"
+IMG = "https://diewland.github.io/special-eureka/doggo.png"
+ATTRS = [
+    # standard
+    #{
+    #  "trait_type": "Collection",
+    #  "value": NAME, 
+    #},
+    # custom
+    #{
+    #  "trait_type": "Rarity",
+    #  "value": "Unrevealed",
+    #},
+]
+ENGINE = "Jigsaw Engine"
+
+metadata = {
+  "name": "***",
+  "description": DESC,
+  "image": "***",
+  "attributes": ATTRS,
+  "compiler": ENGINE,
+}
+
+for id in range(0, TOKEN_SIZE):
+    metadata["name"] = "{} #{}".format(NAME, id)
+    metadata["image"] = IMG.format(id)
+    with open("./{}/{}.json".format(OUTPUT_DIR, START_ID + id), "w") as f:
+        json.dump(metadata, f, ensure_ascii=False)
